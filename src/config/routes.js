2ff76f78019d72458b1bdd7ebc54864b714d3ee5
argo46 <adityaargo4609@gmail.com>
2019-11-16 19:17:29 +0700

    user business logic

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,8 @@ const router = Router()
 
 // Example route
 // End Point = localhost:port/api/v1/example
-router.use('/example', require('../routes/example'))
+router
+	.use('/example', require('../routes/example'))
+	.use('/user', require('../routes/user'))
 
 module.exports = router
