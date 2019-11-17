87d54d3f2d72cd0231d1aca8bfec9b36fe94717f
Nuku Malik Sanjaya Kusuma <nukumalik@gmail.com>
2019-11-18 01:06:48 +0700

    first config commit

--- src/config/routes.js
+++ src/config/routes.js
@@ -4,7 +4,13 @@ const router = Router()
 // Example route
 // End Point = localhost:port/api/v1/example
 router
-	.use('/example', require('../routes/example'))
-	.use('/user', require('../routes/user'))
+	// .use('/example', require('../routes/example'))
+	.use('/users', require('../routes/users'))
+	.use('/deals', require('../routes/deals'))
+	.use('/merchants', require('../routes/merchants'))
+	.use('/payments', require('../routes/payments'))
+	.use('/ppobs', require('../routes/ppobs'))
+	.use('/transactions', require('../routes/transactions'))
+	.use('/transfers', require('../routes/transfers'))
 
 module.exports = router
