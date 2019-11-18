3c852d4e36cc73fc4c2658968f2bacdbdb173f23
argo46 <adityaargo4609@gmail.com>
2019-11-19 02:48:22 +0700

    add balance feature

---
+++ src/controllers/balanceControllers.js
@@ -0,0 +1,90 @@
+// Model
+const Balance = require('../models/Balance')
+const uuid4 = require('uuid/v4')
+
+module.exports = {
+	// Create Balance By ID
+	addBalance: (req, res) => {
+		// Body Fields
+		const { user_id, balance } = req.body
+		const updated_at = new Date()
+		const id = uuid4()
+
+		// Data
+		const data = { id, user_id, balance, updated_at }
+
+		Balance.create(data)
+			.then(result => {
+				res.status(200).json({
+					status: 200,
+					error: false,
+					message: 'Success to create an Balance',
+					data: result,
+				})
+			})
+			.catch(error => {
+				res.status(404).json({
+					status: 404,
+					error: true,
+					message: 'Deal does not exists',
+					data: error,
+				})
+			})
+	},
+
+	// Get Example By ID
+	getDeal: (req, res) => {
+		// Example ID
+		const { user_id } = req.params
+
+		Balance.get(user_id)
+			.then(result => {
+				res.status(200).json({
+					status: 200,
+					error: false,
+					message: 'Success to get deal with ID: ' + user_id,
+					data: result,
+				})
+			})
+			.catch(error => {
+				res.status(404).json({
+					status: 404,
+					error: true,
+					message: 'Deal does not exists',
+					data: error,
+				})
+			})
+	},
+
+	// Update Deal By ID
+	updateDeal: (req, res) => {
+		// Example ID
+		const { user_id } = req.params
+
+		// Body Fields
+		const { balance } = req.body
+
+		// Data
+		const data = {}
+		if (balance) data.code = balance
+		data.updated_at = new Date()
+
+		Balance.update(user_id, data)
+			.then(() => {
+				res.status(200).json({
+					status: 200,
+					error: false,
+					message: 'Success to update deal with User ID: ' + user_id,
+					data,
+				})
+			})
+			.catch(error => {
+				res.status(400).json({
+					status: 400,
+					error: true,
+					message: 'Failed to update deal with User ID: ' + user_id,
+					data: error,
+				})
+			})
+	},
+}
---
+++ src/models/Balance.js
@@ -0,0 +1,48 @@
+const db = require('../config/db')
+
+module.exports = {
+	// Get Balance
+	get: userId => {
+		return new Promise((resolve, reject) => {
+			const sql = `SELECT * FROM balance where user_id='${userId}'`
+
+			db.query(sql, (error, result) => {
+				if (!error) {
+					resolve(result)
+				} else {
+					reject(error)
+				}
+			})
+		})
+	},
+
+	// Create Balance
+	create: data => {
+		return new Promise((resolve, reject) => {
+			const sql = 'INSERT INTO balance SET ?'
+
+			db.query(sql, data, (error, result) => {
+				if (!error) {
+					resolve(result)
+				} else {
+					reject(error)
+				}
+			})
+		})
+	},
+
+	// Update Balance
+	update: (user_id, data) => {
+		return new Promise((resolve, reject) => {
+			const sql = 'UPDATE deals SET ? WHERE user_id = ?'
+
+			db.query(sql, [data, user_id], (error, result) => {
+				if (!error) {
+					resolve(result)
+				} else {
+					reject(error)
+				}
+			})
+		})
+	},
+}
