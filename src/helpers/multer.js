de5add64d2c87c995692548d5463037d547dd22d
argo46 <adityaargo4609@gmail.com>
2019-11-19 02:49:39 +0700

    add logo upload for merchant

--- src/helpers/multer.js
+++ src/helpers/multer.js
@@ -1,17 +1,26 @@
 const multer = require('multer')
 const path = require('path')
 
-var storageMulter = multer.diskStorage({
-	destination: (req,file,callback) => {
+let userStorage = multer.diskStorage({
+	destination: (req, file, callback) => {
 		callback(null, './public/media/img/user_photo')
 	},
 	filename: (req, file, callback) => {
 		callback(null, req.body.name + Date.now() + file.originalname.slice(-4))
-	}
+	},
 })
 
-var fileFilter = (req, file, callback) => {
-	var ext = path.extname(file.originalname)
+let merchantStorage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, './public/media/img/merchant_logo')
+	},
+	filename: (req, file, callback) => {
+		callback(null, req.body.name + Date.now() + file.originalname.slice(-4))
+	},
+})
+
+let fileFilter = (req, file, callback) => {
+	let ext = path.extname(file.originalname)
 	if (ext !== '.png' && ext !== '.jpg' && ext !== '.svg' && ext !== '.jpeg') {
 		callback(null, false)
 		return
@@ -19,4 +28,7 @@ var fileFilter = (req, file, callback) => {
 	callback(null, true)
 }
 
-module.exports = multer({storage: storageMulter, fileFilter})
+module.exports = {
+	userMulter: multer({ storage: userStorage, fileFilter }),
+	merchanMulter: multer({ storage: merchantStorage, fileFilter }),
+}
