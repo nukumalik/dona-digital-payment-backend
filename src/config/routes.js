ed4c127f899879491b95ea84c878e1df55abf81a
Nuku Malik Sanjaya Kusuma <nukumalik@gmail.com>
2019-11-18 23:14:43 +0700

    fix branch config 2

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,7 +3,6 @@ const router = Router()
 
 // Example route
 // End Point = localhost:port/api/v1/example
-<<<<<<< HEAD
 router
 	// .use('/example', require('../routes/example'))
 	.use('/users', require('../routes/users'))
@@ -13,8 +12,5 @@ router
 	.use('/ppobs', require('../routes/ppobs'))
 	.use('/transactions', require('../routes/transactions'))
 	.use('/transfers', require('../routes/transfers'))
-=======
-router.use('/example', require('../routes/example'))
->>>>>>> 6066bf1e292d8619d66da69083c5fb35f4e6c96c
 
 module.exports = router
