f9ef7f83fcd7b73bef33a112157f8dd3070bdce4
argo46 <adityaargo4609@gmail.com>
2019-11-21 09:54:54 +0700

    add database

--- database.sql
+++ database.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Nov 17, 2019 at 12:50 PM
--- Server version: 5.7.27-0ubuntu0.18.04.1
--- PHP Version: 7.2.24-0ubuntu0.18.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2019 at 03:53 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,6 +21,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `dona_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `balance`
+--
+
+CREATE TABLE `balance` (
+  `id` varchar(50) NOT NULL,
+  `user_id` varchar(50) NOT NULL,
+  `Balance` int(20) NOT NULL,
+  `date_updated` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -127,8 +142,38 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `pin`, `photo`) VALUES
+('bfaf6505-6706-4be3-b794-e710971c73ce', 'adit', 'adit@adit.com', '987654321', '$2a$10$LxDOC96nqjUCQDLh2PCNXe5nnpZWdV7Nt6Kn.RxM0Cg5vepxw4ghS', 'public\\media\\img\\user_photo\\adit1574020806570.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_otp`
+--
+
+CREATE TABLE `user_otp` (
+  `id` int(11) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `otp_code` int(6) NOT NULL,
+  `expired_date` date NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT 1,
+  `verify_expiration` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `balance`
+--
+ALTER TABLE `balance`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id_2` (`user_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `deals`
@@ -180,6 +225,14 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `phone` (`phone`);
 
 --
+-- Indexes for table `user_otp`
+--
+ALTER TABLE `user_otp`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_phone_2` (`phone`),
+  ADD KEY `user_phone` (`phone`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -188,26 +241,54 @@ ALTER TABLE `users`
 --
 ALTER TABLE `deals`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `merchants`
 --
 ALTER TABLE `merchants`
   MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `ppobs`
 --
 ALTER TABLE `ppobs`
   MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `transfers`
 --
 ALTER TABLE `transfers`
   MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_otp`
+--
+ALTER TABLE `user_otp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `balance`
+--
+ALTER TABLE `balance`
+  ADD CONSTRAINT `user_id_balance` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `user_otp`
+--
+ALTER TABLE `user_otp`
+  ADD CONSTRAINT `otp_user_phone` FOREIGN KEY (`phone`) REFERENCES `users` (`phone`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
