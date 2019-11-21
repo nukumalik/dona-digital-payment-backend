f47d7152337cabe7a92700d6e34bc9e1ca05c325
argo46 <adityaargo4609@gmail.com>
2019-11-21 09:38:47 +0700

    add add balance

--- src/controllers/balanceControllers.js
+++ src/controllers/balanceControllers.js
@@ -1,6 +1,35 @@
 const Balance = require('../models/Balance')
 
 module.exports = {
+	// Create Balance By ID
+	addBalance: (req, res) => {
+		// Body Fields
+		const { user_id, balance } = req.body
+		const updated_at = new Date()
+		const id = uuid4()
+
+		// Data
+		const data = { id, user_id, balance, updated_at }
+
+		Balance.create(data)
+			.then(result => {
+				res.status(200).json({
+					status: 200,
+					error: false,
+					message: 'Success to create an Balance',
+					data: result,
+				})
+			})
+			.catch(error => {
+				res.status(404).json({
+					status: 404,
+					error: true,
+					message: 'Deal does not exists',
+					data: error,
+				})
+			})
+	},
+
 	getBalance: (req, res) => {
 		// User ID
 		const { user_id } = req.params
